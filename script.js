5ae0820f227615f18c390b973fb17399d86fe449
Victor <wortsac01@gmail.com>
2025-07-03 18:03:16 -0300

    ajustments

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let input = document.querySelectorAll(".numbers-only")
 
 // formatting inputs to accept only numbers.
 input.forEach((input) => {
-  input.addEventListener("input", () => {
+  input.addEventListener('input', () => {
     let value = input.value.replace(/\D/g, "")
     input.value = value
     
